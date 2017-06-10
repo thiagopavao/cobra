96d599bdbdeb9373d56a72c7a78c0f8be86db655
thiagopavao <thiagoppavao19@gmail.com>
2017-06-10 12:30:22 -0300

    a cobrinha anda para o lado do comunismo

--- cobra.js
+++ cobra.js
@@ -5,10 +5,12 @@ escala = 20;
 
 function setup() {
     createCanvas(tamanho,tamanho);
+    frameRate(60);
     
     cobrinha = {
         cor: color(255,0,0),
         corpo: [],
+        dir: createVector(-20,0),
         cria: function() {
             for(i=0;i<4;i++) {
                 x= tamanho /2 + i*escala;
@@ -19,11 +21,19 @@ function setup() {
         },
         desenha: function() {
             fill(this.cor)
+            this.anda();
             for(i=0;i<this.corpo.length;i++){
                 q = this.corpo[i];
                 rect(q.x,q.y,escala,escala);
             }
-    }
+        },
+        anda: function() {
+            q= this.corpo[0].copy();
+            q.add(this.dir);
+            this.corpo.unshift(q);
+            
+            this.corpo.splice(-1,1);
+        }
   };
     
     cobrinha.cria();
