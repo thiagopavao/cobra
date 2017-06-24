7745ea521a05938a9801dc4034b3a027d531db3b
thiagopavao <thiagoppavao19@gmail.com>
2017-06-24 12:41:09 -0300

    a cobrinha tem opcao politica

--- cobra.js
+++ cobra.js
@@ -3,14 +3,21 @@ escala = 20;
 
 
 
+
 function setup() {
     createCanvas(tamanho,tamanho);
     frameRate(60);
+    dirs = {
+        'a': createVector(-1,0),
+        'w': createVector(0,-1),
+        's': createVector(0,1),
+        'd': createVector(1,0),
+     };
     
     cobrinha = {
         cor: color(255,0,0),
         corpo: [],
-        dir: createVector(-20,0),
+        dir: createVector(0,0),
         cria: function() {
             for(i=0;i<4;i++) {
                 x= tamanho /2 + i*escala;
@@ -18,6 +25,7 @@ function setup() {
                 q = createVector(x,y);
                 this.corpo.push(q);
             }
+            this.setDir(dirs['a']);
         },
         desenha: function() {
             fill(this.cor)
@@ -33,7 +41,14 @@ function setup() {
             this.corpo.unshift(q);
             
             this.corpo.splice(-1,1);
-        }
+        },
+        setDir : function(d) {
+            angulo = degrees(p5.Vector.angleBetween(this.dir,d))
+            if (angulo != 180) {
+            this.dir = d.copy();
+            this.dir.mult(escala);
+            }
+            }
   };
     
     cobrinha.cria();
@@ -43,3 +58,15 @@ function draw() {
     background(0);
     cobrinha.desenha();
 }
+
+function keyPressed() {
+    switch(key.toLowerCase()){
+        case 'w':
+        case 'a':
+        case 's':
+        case 'd':
+            d = dirs[key.toLowerCase()];
+            cobrinha.setDir(d);
+            break;
+    }
+} 
